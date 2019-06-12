41dcfb8f6bb54e41062f1f09eded1c727d729e20
lucaam <luca.amoriello@hotmail.it>
2019-06-12 10:54:28 +0200

    Typing animation, arrays to store data. Improved existing file check.

--- scripts/terminal.js
+++ scripts/terminal.js
@@ -1,23 +1,80 @@
+
 $(function() {
-  var files = ["curriculum_vitae.pdf", "focusit_master.zip"];
+  var anim = false;
+    function typed(finish_typing) {
+        return function(term, message, delay, finish) {
+            anim = true;
+            var prompt = term.get_prompt();
+            var c = 0;
+            if (message.length > 0) {
+                term.set_prompt('');
+                var new_prompt = '';
+                var interval = setInterval(function() {
+                    var chr = $.terminal.substring(message, c, c+1);
+                    new_prompt += chr;
+                    term.set_prompt(new_prompt);
+                    c++;
+                    if (c == length(message)) {
+                        clearInterval(interval);
+                        // execute in next interval
+                        setTimeout(function() {
+                            // swap command with prompt
+                            finish_typing(term, message, prompt);
+                            anim = false
+                            finish && finish();
+                        }, delay);
+                    }
+                }, delay);
+            }
+        };
+    }
+    function length(string) {
+        string = $.terminal.strip(string);
+        return $('<span>' + string + '</span>').text().length;
+    }
+    var typed_prompt = typed(function(term, message, prompt) {
+        term.set_prompt(message + ' ');
+        term.exec('help')
+    });
+    var typed_message = typed(function(term, message, prompt) {
+        term.echo(message)
+        term.set_prompt(prompt);
+    });
+
+
+  var files = ["curriculum_vitae.pdf",
+               "focusit_master.zip"];
+
+  var commands = ["ls - show files available for download",
+                  "download - download a file (see files with ls)",
+                  'youtube - open my youtube channel',
+                  'twitter - open my twitter profile',
+                  'instagram - open my instagram profile',
+                  'telegram - contact me on telegram',
+                  'facebook - open my facebook profile',
+                  'send_email - email me',
+                  'clear - clear console text'];
 
     $('#terminal').terminal({
       help: function() {
-        this.echo('help - show this help message');
-        this.echo('ls - show files available for download');
-        this.echo('download - download a file, for example download curriculum_vitae.pdf');
-        this.echo('youtube - open my youtube channel');
-        this.echo('twitter - open my twitter profile');
-        this.echo('instagram - open my instagram profile');
-        this.echo('telegram - contact me on telegram');
-        this.echo('facebook - open my facebook profile');
-        this.echo('send_email - email me');
-        this.echo('clear - clear console text');
+        for(i=0; i<commands.length; i++){
+          this.echo(commands[i]);
+        }
+        // this.echo('ls - show files available for download');
+        // this.echo('download - download a file (see files with ls)');
+        // this.echo('youtube - open my youtube channel');
+        // this.echo('twitter - open my twitter profile');
+        // this.echo('instagram - open my instagram profile');
+        // this.echo('telegram - contact me on telegram');
+        // this.echo('facebook - open my facebook profile');
+        // this.echo('send_email - email me');
+        // this.echo('clear - clear console text');
       },
 
       ls: function() {
-        this.echo('curriculum_vitae.pdf');
-        this.echo('focusit_master.zip')
+        for(i=0; i<files.length; i++){
+          this.echo(files[i]);
+        }
       },
 
       youtube: function() {
@@ -53,7 +110,7 @@ $(function() {
       download: function(filename) {
         var done = 0;
         var filepath = "/download/";
-        if(files.includes(filename)){
+        if(files.indexOf(filename)!=-1){
           if (filename == 'focusit_master.zip'){
             window.open("https://github.com/lucaam/focus.it/archive/master.zip");
             done = 1;
@@ -68,8 +125,21 @@ $(function() {
       },
     }, {
       prompt: 'user@lucaamoriello: ',
-      greetings: "Welcome on lucaamoriello.it. Please, type [[b;red;]'help'] to get started!",
+      greetings: null,
       height: 300,
       completion: true,
+      onInit: function(term) {
+            // first question
+            var msg = "Welcome to lucaamoriello.it! Type [[b;red;]'help'] to get started!";
+            typed_message(term, msg, 40, function() {
+                typed_prompt(term, "user@lucaamoriello: ", 40);
+            });
+        },
+        keydown: function(e) {
+            //disable keyboard when animating
+            if (anim) {
+                return false;
+            }
+        }
     });
 });
