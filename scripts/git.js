40aa152c5ebe7209382554fa331060463859834f
lucaam <luca.amoriello@hotmail.it>
2019-06-12 12:27:31 +0200

    Check if API limit reached for current IP.

--- scripts/git.js
+++ scripts/git.js
@@ -8,16 +8,19 @@ jQuery.fn.loadRepositories = function(username) {
     var target = this;
     $.githubUser(username, function(data) {
         var repos = data.data; // JSON Parsing
-        sortByName(repos);
-
+        console.log(repos.message);
         var list = $('<dl/>');
         target.empty().append(list);
-        $(repos).each(function() {
-            if (this.name != (username.toLowerCase()+'.github.com')) {
-                list.append('<dt><span class="my-text-gray" href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</span> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
-                list.append('<dd>' + this.description +'</dd>');
-            }
-        });
+        if(repos.message.includes("API rate limit exceeded for")){
+          $("#my-github-projects").append("<span class='my-text-grey'> API limit reached, please try later.</span>");
+          return;
+        }
+          $(repos).each(function() {
+              if (this.name != (username.toLowerCase()+'.github.com')) {
+                  list.append('<dt><span class="my-text-gray" href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</span> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
+                  list.append('<dd>' + this.description +'</dd>');
+              }
+          });
       });
 
     function sortByName(repos) {
