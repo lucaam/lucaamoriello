5f1a5fd02ccc6f95dab19a87478296debc38074d
Luca Amoriello <luca.amoriello@hotmail.it>
2019-06-12 23:56:32 +0200

    Initial picture gallery logic.

---
+++ scripts/gallery.js
@@ -0,0 +1,30 @@
+
+$(document).ready( function() {
+  var defaultPath = './images/';
+  var imgNumber = 15;
+  var defaultName = "img";
+  var defaultExt = ".jpg";
+  var i = 1;
+
+  $('#heregoesthepic').attr("style", 'background-image: url("' + defaultPath + defaultName + i++ + defaultExt + '");');
+
+     $('#newImage').click(function nextImage() {
+       console.log('next');
+       $('#heregoesthepic').fadeOut('fast', function backIn() {
+         $('#heregoesthepic').removeAttr( 'style' );
+         $('#heregoesthepic').attr("style", 'background-image: url("' + defaultPath + defaultName + i++ + defaultExt + '");');
+         $('#heregoesthepic').fadeIn("slow");
+
+       });
+
+
+       if(i>=imgNumber)
+       {
+         i = 1;
+       }
+       console.log('sono in next ed i = ' + i);
+
+
+     });
+
+ });
