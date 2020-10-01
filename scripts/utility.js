c7922b164c689613bc7e67cd3206ada6b55f43c3
Luca Amoriello <luca.amoriello@hotmail.it>
2020-10-01 20:31:56 +0200

    Sticky navbar and rewriter

--- scripts/utility.js
+++ scripts/utility.js
@@ -11,3 +11,44 @@ function writeAge() {
 window.addEventListener('load', function() {
     writeAge();
 })
+
+
+/**
+ *
+ *	Sticky navbar on scroll.
+ *
+ */
+var navbar = $("#navbar");
+var navbarContainer = $("#navbar-container");
+
+var header = $("#navbar").offset().top;
+var about = $("#about-me").offset().top;
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > header) {
+        navbar.addClass("static-top fixed-top container p-2 ");
+
+    } else {
+        navbar.removeClass("static-top fixed-top container p-2");
+    }
+
+    if ($(this).scrollTop() > about) {
+        navbarContainer.addClass("container-fluid bg-secondary");
+
+    } else {
+        navbarContainer.removeClass("container-fluid bg-secondary");
+    }
+});
+
+/*
+ *
+ *	Url rewrite when clicking an anchor.
+ *
+ */
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+        setTimeout(() => {
+            history.pushState('', document.title, window.location.pathname);
+        }, 100);
+    });
+});
